4a1112550f29fe6005285ea1d0ff6c408c9c9a2f
cezarj59 <cezarj59@yahoo.com>
2023-09-08 23:53:33 -0300

    feature - add @GetMapping metodo listar()

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,7 +1,10 @@
 package med.voll.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
@@ -24,5 +28,10 @@ public class MedicoController {
 	public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
 		System.out.println(dados);
 		repository.save(new Medico(dados));
+	}
+	
+	@GetMapping
+	public List<DadosListagemMedico> listar(){
+		return repository.findAll().stream().map(DadosListagemMedico::new).toList();
 	}
 }
