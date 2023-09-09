31f04a6277e4a8dcab26ebe725990bf8f0b4de47
cezarj59 <cezarj59@yahoo.com>
2023-09-08 21:09:46 -0300

    feature - use PacienteRepository

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,18 +1,29 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import med.voll.api.paciente.DadosCadastroPaciente;
+import med.voll.api.paciente.Paciente;
+import med.voll.api.paciente.PacienteRepository;
 
-@RestController
-@RequestMapping("pacientes")
+@RestController // Indica que esta classe é um controlador Rest que lida com requisições HTTP
+@RequestMapping("pacientes") // Define o mapeamento da URL base para este controlador como "/pacientes"
 public class PacienteController {
 
-	@PostMapping
-	public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-		System.out.println(dados);
-	}
+    @Autowired
+    private PacienteRepository repository; // Injeta uma instância de PacienteRepository para interagir com o banco de dados
+
+    @PostMapping // Mapeia a operação HTTP POST para este método
+    @Transactional // Indica que esta operação é transacional, ou seja, envolve uma transação com o banco de dados
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        System.out.println(dados); // Imprime os dados recebidos no console (pode ser usado para fins de depuração)
+        repository.save(new Paciente(dados)); // Cria um novo objeto Paciente com base nos dados recebidos e o salva no banco de dados
+    }
 }
+
